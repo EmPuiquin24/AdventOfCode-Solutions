9e796b43da04a1a677e3f2ce6d74aa59e62824d7
EmPuiquin24 <hepuiquin@proton.me>
2024-12-04 15:26:10 -0500

    Part B added

--- Year 2024/Day 2/solution2.cpp
+++ Year 2024/Day 2/solution2.cpp
@@ -7,28 +7,39 @@
 using namespace std;
 
 bool isSafeReport(vector<int> Vector) {
-	bool isDecreasing = true;
-	bool isIncreasing  = true;
+	bool isIncreasing = (Vector[1] - Vector[0] > 0);
+	bool isDecreasing = (Vector[1] - Vector[0] < 0);
 
 	for(int i = 0; i < Vector.size() - 1; i++) {
-		int levelDifference = Vector[i] - Vector[i+1];
+		int levelDifference = Vector[i+1] - Vector[i];
 			
 		if (abs(levelDifference) < 1 || abs(levelDifference) > 3) {
 			return false;
 		}
-
-		if (levelDifference < 0) {
-			isDecreasing = false;
+		if (levelDifference < 0 &&  isIncreasing) {
+			return false;	
 		}
-		else {
-			isIncreasing = false;
+		if (levelDifference > 0 && isDecreasing)
+			return false;	
 		}
+	return true;
+}
 
-		if (!isDecreasing && !isIncreasing) {	
-			return false;
+bool isDampenerReport(vector<int> Vector) {
+	// Fuerza Bruta aaaaaah algorithm
+	if (isSafeReport(Vector)) {
+		return true;
+	}
+
+	for (int i = 0; i < Vector.size() ; i++) {
+		vector<int> Vector2 = Vector;
+		Vector2.erase(Vector2.begin() + i);
+
+		if (isSafeReport(Vector2)) {
+			return true;
 		}
 	}
-	return true;
+	return false;
 }
 
 void countReports(string Filename, int& nSafeReports) {
@@ -49,14 +60,49 @@ void countReports(string Filename, int& nSafeReports) {
 			nSafeReports++;	
 		}
 	}
+
+	file.close();
+}
+
+
+void countDampenerReports(string Filename, int& nSafeReports) {
+	
+	ifstream file2(Filename);
+	string row;
+
+	while (getline(file2, row)) {
+		int number;
+		vector<int> Vector;
+		stringstream ss(row);
+		
+		while(ss >> number) {
+			Vector.push_back(number);
+		}
+
+		if (isDampenerReport(Vector)) {
+			nSafeReports++;	
+		}
+	}
+	
+	file2.close();
 }
 
 int main() {
 	string Filename = "input.txt";
+
+	// Part 1
 	int nSafeReports = 0;
 	
 	countReports(Filename, nSafeReports);
-	
+
 	cout << "The amount of safe reports are: " << nSafeReports << endl;
+	
+	// Part 2
+	int nDampenerReports= 0;
+
+	countDampenerReports(Filename, nDampenerReports);
+
+	cout << nDampenerReports << endl;
+
 	return 0;
 }
